31099cd0007316d1ae25784a858a48bd8cfb69bc
Vicente <vicente_tomi@hotmail.com>
2021-08-10 13:26:17 -0400

    server updated

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(morgan("dev"));
